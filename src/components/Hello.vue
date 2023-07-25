284253eca596c7ec673be6ea58e2d2ba5bdcbee0
Allen-zc <1053831730@qq.com>
2023-07-25 09:24:55 +0800

    gulimall-vue-pro 代码第一次提交 p45

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <dev>
+        <h1>你好，hello,{{ name }}</h1>
+        <el-radio v-model="radio" label="1">备选项1</el-radio>
+        <el-radio v-model="radio" label="2">备选项2</el-radio>
+    </dev>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            name:"张三",
+            radio: "2"
+        }
+    }
+}
+</script>
